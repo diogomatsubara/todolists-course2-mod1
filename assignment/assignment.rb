5572e7f38637f59e422b9fd69a653fd36d66efd0
Diogo Matsubara <diogo.matsubara@gmail.com>
2017-04-03 08:15:24 -0300

    add solution for rq07.1: implement assignment.update_password

--- assignment/assignment.rb
+++ assignment/assignment.rb
@@ -80,6 +80,9 @@ class Assignment
       # accept an id and password_digest input parameters
       # use the User Model class to update the `password_digest` for the User associated with the id primary key
       # (no return is required)
+    user = User.find(id)
+    user.password_digest = password_digest
+    user.save()
   end
 
   def update_listname(id, name)
